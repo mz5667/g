1d675e02f8b45c0f5c856721e6d40ee040c039a6
alex <aok@buran.rest>
2022-03-07 10:19:39 +0300

    v3

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,18 +1,17 @@
 package app;
 
+import controls.Label;
 import io.github.humbleui.jwm.*;
 import io.github.humbleui.jwm.skija.EventFrameSkija;
 import io.github.humbleui.skija.Canvas;
-import io.github.humbleui.skija.Paint;
-import io.github.humbleui.skija.RRect;
 import io.github.humbleui.skija.Surface;
 import misc.CoordinateSystem2i;
-import misc.Misc;
 
 import java.io.File;
 import java.util.function.Consumer;
 
 import static app.Colors.APP_BACKGROUND_COLOR;
+import static app.Colors.PANEL_BACKGROUND_COLOR;
 
 /**
  * Класс окна приложения
@@ -26,11 +25,30 @@ public class Application implements Consumer<Event> {
      * отступы панелей
      */
     private static final int PANEL_PADDING = 5;
+    /**
+     * радиус скругления элементов
+     */
+    public static final int C_RAD_IN_PX = 4;
+    /**
+     * Первый заголовок
+     */
+    private final Label label;
+    /**
+     * Первый заголовок
+     */
+    private final Label label2;
+    /**
+     * Первый заголовок
+     */
+    private final Label label3;
+
 
     /**
      * Конструктор окна приложения
      */
     public Application() {
+
+
         // создаём окно
         window = App.makeWindow();
         // задаём обработчиком событий текущий объект
@@ -47,6 +65,18 @@ public class Application implements Consumer<Event> {
             case WINDOWS -> window.setIcon(new File("src/main/resources/windows.ico"));
             case MACOS -> window.setIcon(new File("src/main/resources/macos.icns"));
         }
+
+        // создаём первый заголовок
+        label = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING,
+                4, 4, 1, 1, 1, 1, "Привет, мир!", true, true);
+
+        // создаём второй заголовок
+        label2 = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING,
+                4, 4, 0, 3, 1, 1, "Второй заголовок", true, true);
+
+        // создаём третий заголовок
+        label3 = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING,
+                4, 4, 2, 0, 1, 1, "Это тоже заголовок", true, true);
 
         // названия слоёв, которые будем перебирать
         String[] layerNames = new String[]{
@@ -103,22 +133,12 @@ public class Application implements Consumer<Event> {
         canvas.save();
         // очищаем канвас
         canvas.clear(APP_BACKGROUND_COLOR);
-
-        // восстанавливаем состояние канваса
-        canvas.restore();
-        // координаты левого верхнего края окна
-        int rX = windowCS.getSize().x / 3;
-        int rY = windowCS.getSize().y / 3;
-        // ширина и высота
-        int rWidth =  windowCS.getSize().x  / 3;
-        int rHeight = windowCS.getSize().y  / 3;
-        // создаём кисть
-        Paint paint = new Paint();
-        // задаём цвет рисования
-        paint.setColor(Misc.getColor(100, 255, 255, 255));
-        // рисуем квадрат
-        canvas.drawRRect(RRect.makeXYWH(rX, rY, rWidth, rHeight, 4), paint);
-
+        // рисуем заголовок
+        label.paint(canvas, windowCS);
+        // рисуем второй заголовок
+        label2.paint(canvas, windowCS);
+        // рисуем третий заголовок
+        label3.paint(canvas, windowCS);
         // восстанавливаем состояние канваса
         canvas.restore();
     }
